9c8174c5a428dd9ffdd5e96db2270924b71218de
EdrowsLuo <793971297@qq.com>
2018-05-03 21:55:02 +0800

    ？？？

---
+++ EdGameFramework/src/main/java/com/edplan/framework/graphics/opengl/bufferObjects/VertexBufferObject.java
@@ -0,0 +1,9 @@
+package com.edplan.framework.graphics.opengl.bufferObjects;
+import android.opengl.GLES20;
+
+public class VertexBufferObject
+{
+	public VertexBufferObject(){
+		//GLES20.glGenBuffers(GLES20.GL_VERTEX_ATTRIB_ARRAY_BUFFER_BINDING
+	}
+}
